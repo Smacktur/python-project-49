e86f90599260b5d32100847780f81ae2ff62eb57
Smacktur <smacktur@gmail.com>
2022-10-23 23:04:36 +0300

    add utils for engine and future games
    * for seamless interaction of games with the engine

---
+++ games_brain/utils/randomizer.py
@@ -0,0 +1,11 @@
+from random import randint, choice
+
+
+def get_rand_int(range_to: int):
+    '''Returns a random value from 1 to the specified range'''
+    return randint(1, range_to)
+
+
+def get_rand_char(_list):
+    ''''Returns a random item from the list'''
+    return random.choice(_list)
